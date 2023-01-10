6d74af4f8bdb7f76ed24b00c0de058c47255a6bf
Szymon <szymon.lis1507@gmail.com>
2023-01-10 18:50:28 +0100

    Add Mongo Client, Database and schema for collections in MongoDbContext

--- PowerQualityManageService.Infrastructure/MongoDBInfrastructure/Concrete/MongoDbContext.cs
+++ PowerQualityManageService.Infrastructure/MongoDBInfrastructure/Concrete/MongoDbContext.cs
@@ -12,11 +12,17 @@ namespace PowerQualityManageService.Infrastructure.MongoDBInfrastructure.Concret
 
 public class MongoDbContext : IMongoDbContext
 {
-    private readonly MongoClient
+    private readonly MongoClient _mongoClient;
+    private readonly IMongoDatabase _mongoDatabase;
+    // private readonly IMongoCollection<Type> _collectionName;
     public MongoDbContext(IOptions<MongoDbConfig> mongoDbConfig)
     {
-
+        _mongoClient = new MongoClient(mongoDbConfig.Value.ConnectionString);
+        _mongoDatabase = _mongoClient.GetDatabase(mongoDbConfig.Value.Database);
+        //_collectionName = _mongoDatabase.GetCollection<Type>(mongoDbConfig.Value.Collection)
     }
+
+    //public IMongoCollection<Type> CollectionName => _collectionName;
     public bool IsMongoActive()
     {
         throw new NotImplementedException();
