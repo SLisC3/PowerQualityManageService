ce234979022dfdc856e63ec26411c50977ee5f55
Szymon <szymon.lis1507@gmail.com>
2023-01-10 19:36:10 +0100

    Add controller schema

---
+++ PowerQualityManageService/Controllers/DataController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PowerQualityManageService.Controllers;
+public class DataController : Controller
+{
+    // GET: DataController
+    public ActionResult Index()
+    {
+        return View();
+    }
+
+    // POST: DataController/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+}
