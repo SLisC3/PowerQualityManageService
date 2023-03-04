2efb976e8e8bed0f18b8adf6b3cb63ca7faf5f11
Szymon <szymon.lis1507@gmail.com>
2023-03-04 17:34:51 +0100

    Add Extension to Update Value

---
+++ PowerQualityManageService.Core/Utils/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerQualityManageService.Core.Utils.Extensions;
+public static class EnumerableExtensions
+{
+    public static IEnumerable<T> SetValues<T>( this IEnumerable<T> items, Action<T> update)
+    {
+        foreach (var item in items)
+        {
+            update(item);
+        }
+        return items;
+    }
+}
