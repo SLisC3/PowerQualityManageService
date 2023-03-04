0837709ce1ddf6f8dad6e6fd647cfe30e0a2130d
Szymon <szymon.lis1507@gmail.com>
2023-03-04 17:29:57 +0100

    Add helper for ParsingColumnHeaders

---
+++ PowerQualityManageService.Core/Helpers/ColumnHeaderRegexHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerQualityManageService.Core.Helpers;
+internal class ColumnHeaderRegexHelper
+{
+}
