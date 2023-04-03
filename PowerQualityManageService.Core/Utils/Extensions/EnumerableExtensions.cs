c1cabefa2db2a4c054d3e5e10ad81fd5348b2999
Szymon <szymon.lis1507@gmail.com>
2023-04-03 19:47:17 +0200

    Change Enumerable Extensions to set new values with update Func

--- PowerQualityManageService.Core/Utils/Extensions/EnumerableExtensions.cs
+++ PowerQualityManageService.Core/Utils/Extensions/EnumerableExtensions.cs
@@ -7,12 +7,8 @@ using System.Threading.Tasks;
 namespace PowerQualityManageService.Core.Utils.Extensions;
 public static class EnumerableExtensions
 {
-    public static IEnumerable<T> SetValues<T>( this IEnumerable<T> items, Func<T,T> update)
+    public static IEnumerable<T> SetValues<T>(this IEnumerable<T> items, Func<T, T> update)
     {
-        foreach (var item in items)
-        {
-            item = update(item);
-        }
-        return items;
+        return items.Select(x=> update(x));
     }
 }
