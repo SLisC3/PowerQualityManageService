7ece06e58429aaf2cd3cb059193c3eb1adf1eaae
Szymon <szymon.lis1507@gmail.com>
2023-04-08 15:35:11 +0200

    Modify methods for not alowing null reference from Reading stream, Create method for Reading exact number of rows

--- PowerQualityManageService.Core/Helpers/CSVHelper.cs
+++ PowerQualityManageService.Core/Helpers/CSVHelper.cs
@@ -1,22 +1,54 @@
-﻿using System.Data;
+﻿using PowerQualityManageService.Core.Helpers;
+using System.Data;
+using System.Reflection.PortableExecutable;
 
 namespace PowerQualityManageService;
 
 public static class CSVHelper
 {
+    public static DataTable ReadRowsCount(Stream stream, List<string> headers, int rowsCount)
+    {
+        DataTable dt = new DataTable();
+        headers.ForEach(x => dt.Columns.Add(x));
+
+        if(stream.Position != 0) { stream.Position = 0; }
+        using (NoDisposeInputStreamReader sr = new NoDisposeInputStreamReader(stream))
+        {
+            if (sr.EndOfStream) return dt;
+            sr.ReadLine();
+            for (int i = 0; i < rowsCount; i++)
+            {
+                if (sr.EndOfStream) return dt;
+                var row = sr.ReadLine();
+                if (row == null) return dt; ;
+                string[] rows = row.Split(';');
+                DataRow dr = dt.NewRow();
+                for (int j = 0; j < rows.Length; j++)
+                {
+                    dr[j] = rows[j];
+                }
+                dt.Rows.Add(dr);
+            }
+        }
+        return dt;
+    }
     public static DataTable ConvertCSVtoDataTable(Stream stream)
     {
         DataTable dt = new DataTable();
-        using (StreamReader sr = new StreamReader(stream))
+        using (NoDisposeInputStreamReader sr = new NoDisposeInputStreamReader(stream))
         {
-            string[] headers = sr.ReadLine().Split(',');
-            foreach (string header in headers)
+            var header = sr.ReadLine();
+            if (header == null) return dt; ;
+            string[] headers = header.Split(';');
+            foreach (string h in headers)
             {
-                dt.Columns.Add(header);
+                dt.Columns.Add(h);
             }
             while (!sr.EndOfStream)
             {
-                string[] rows = sr.ReadLine().Split(',');
+                var row = sr.ReadLine();
+                if (row == null) return dt; ;
+                string[] rows = row.Split(';');
                 DataRow dr = dt.NewRow();
                 for (int i = 0; i < headers.Length; i++)
                 {
@@ -29,10 +61,10 @@ public static class CSVHelper
     }
     public static List<string> ReadHeaders(Stream stream)
     {
-        using (StreamReader sr = new StreamReader(stream))
+        using (NoDisposeInputStreamReader sr = new NoDisposeInputStreamReader(stream))
         {
             var headerstring = sr.ReadLine();
-            if (headerstring != null) { return headerstring.Split(new char[] { ',', ';' }).ToList(); }        
+            if (headerstring != null) { return headerstring.Split(new char[] { ',', ';' }).ToList(); }
         }
         return new List<string>();
     }
