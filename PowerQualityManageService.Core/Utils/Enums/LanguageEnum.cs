1a1adc4e462aad222b8983e050f6557c8d02a130
Szymon <szymon.lis1507@gmail.com>
2023-04-28 21:58:33 +0200

    Merge Master branch into Resources

---
+++ PowerQualityManageService.Core/Utils/Enums/LanguageEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerQualityManageService.Core.Utils.Enums;
+public enum LanguageEnum
+{
+    en = 0,
+    pl = 1
+}
