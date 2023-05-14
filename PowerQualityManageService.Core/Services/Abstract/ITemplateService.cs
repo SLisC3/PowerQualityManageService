513c274905621b3f81e2809be1d0e37ccea7bfbd
Szymon <szymon.lis1507@gmail.com>
2023-05-14 13:56:32 +0200

    Add Repositories + Services + Contoller for basic operations with Template

---
+++ PowerQualityManageService.Core/Services/Abstract/ITemplateService.cs
@@ -0,0 +1,12 @@
+﻿using PowerQualityManageService.Infrastructure.Models;
+
+
+namespace PowerQualityManageService.Core.Services.Abstract;
+
+public interface ITemplateService
+{
+    Task<Template> AddTemplate(Template template);
+    Task<bool> DeleteTemplate(int id);
+    Task<Template?> EditTemplate(int id, Template template);
+    Task<Template?> GetTemplateById(int id);
+}
